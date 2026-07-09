4c2892e0fa98c1d8aac554542ed1966d35f0ced6
vedhagsvp <vedhag230223@gmail.com>
2026-07-09 23:33:59 +0530

    Delete trainer/preprocess.py

--- trainer/preprocess.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def run_preprocessing(df: pd.DataFrame) -> pd.DataFrame:
-    # Example cleaning logic
-    df = df.dropna()
-    df.columns = [c.lower() for c in df.columns]
-    
-    return df
